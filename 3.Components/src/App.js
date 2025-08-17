48d30f2d4e7ba5e23b98d98512415e12db750188
Bhagyashri1301 <bhagyashrip1998@gmail.com>
2025-08-17 18:51:35 +0530

    2.JSX

---
+++ 3.Components/src/App.js
@@ -0,0 +1,14 @@
+import List from './List';
+
+function App() {
+  let name = 'Bhagyashri';
+  return (
+    <>
+      <h1>Welcome React {name}</h1>
+      <List />
+      <h2>Hi</h2>
+    </>
+  );
+}
+
+export default App;
